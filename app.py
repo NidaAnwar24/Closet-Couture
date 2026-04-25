03da9122ba50df0b8e4129465079fe6b05ba72f1
NidaAnwar24 <nidanwar2405@gmail.com>
2026-04-25 13:24:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -362,4 +362,5 @@ with app.app_context():
 if __name__ == '__main__':
     os.makedirs(os.path.join('static','uploads','clothing'), exist_ok=True)
     os.makedirs(os.path.join('static','uploads','outfits'),  exist_ok=True)
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
